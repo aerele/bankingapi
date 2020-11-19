337e7812c2f22fbc45438d22cc241f9fab0aad31
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2020-11-19 10:40:27 +0530

    test api implementation

---
+++ banking_api/__init__.py
@@ -0,0 +1,2 @@
+from banking_api.test import Test
+from banking_api.common_config import CommonConfig
---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+setup(
+    name='bankingapi',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Banking API Integration',
+    author='Aerele',
+    license='MIT',
+)
