8c9e8b595b60218fa616040409a13f56b4815866
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-03-13 17:20:32 +0530

    json altered

--- banking_api/json_bank.py
+++ banking_api/json_bank.py
@@ -11,50 +11,50 @@ class JsonBank(object):
 	
 	def intiate_transaction(self):
 		res = {
-			 "head": {
-		 		"responseTimestamp": "1526969112101",
-		 		"version": "v1",
-		 		"clientId": "C11",
-		 		"signature": "TXBw50YPUKIgJd8gR8RpZuOMZ+csvCT7i0/YXmG//J8+BpFdY5goPBiLAkCzKlCkOvAQip/Op5aD6Vs+cNUTjFmC55JBxvp7WunZ45Ke2q0="
-			 	},
-			 "body": {
-			 	"resultInfo": {
-			 	"resultStatus": "S",
-			 	"resultCode": "0000",
-			 	"resultMsg": "Success"
-			 	},
-			 	"txnToken": "fe795335ed3049c78a57271075f2199e1526969112097",
-			 	"isPromoCodeValid": false,
-			 	"authenticated": false
-			 	}
+			"head": {
+				"responseTimestamp": "152696112101",
+				"version": "v1",
+				"clientId": "C11",
+				"signature": "TXBw50YPUK///fwefewegegre"
+			},
+			"body":{
+				"resultInfo":{
+					"resultStatus": "S",
+					"resultCode": "0000",
+					"resultMsg": "Success"
+				},
+				"txnToken": "fe8798999efewf889090erfre",
+				"isPromoCodeValid": false,
+				"authenticated": false
+			}
 		}
 		return res["body"]["resultInfo"]["resultStatus"]
 
 	def transaction_status(self):
 		res = {
 			"head": {
-				"responseTimestamp": "1553496322922",
+				"responseTimestamp": "152696112101",
 				"version": "v1",
 				"clientId": "C11",
-				"signature": "xxxxx"
+				"signature": "TXBw50YPUK///fwefewegegre"
 			},
 			"body": {
-				"resultInfo": {
+				"resultInfo":{
 					"resultStatus": "TXN_SUCCESS",
 					"resultCode": "01",
 					"resultMsg": "Txn Success"
 				},
-				"txnId": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-				"bankTxnId": "xxxxxxxxxxxxxxx",
-				"orderId": "xxxxxxx",
+				"txnId": "xxxxxxxxxxxxxxxxxxxxxxxxx",
+				"bankTxnId": "xxxxxxxxxx",
+				"orderId": "xxxxxxxx",
 				"txnAmount": "100.00",
 				"txnType": "SALE",
 				"gatewayName": "HDFC",
 				"bankName": "HSBC",
-				"mid": "xxxxxxxxxxxxxxxxxxxx",
+				"mid": "xxxxxxxxxxxxxx",
 				"paymentMode": "CC",
 				"refundAmt": "100.00",
 				"txnDate": "2019-02-20 12:35:20.0"
 			}
-			} 
+		}
 		return res["body"]["resultInfo"]["resultStatus"]
